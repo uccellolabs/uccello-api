5b5caa80fb5e9d1d09d8f701b4bc92d9dcb07a3e
Jonathan SARDO <sardoj@gmail.com>
2019-08-23 10:33:36 +0200

    fix(route): Changes middleware

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -16,14 +16,14 @@ Route::name('api.uccello.')
     Route::get('auth/me', 'ApiAuthController@me')->name('auth.me');
     Route::get('auth/refresh', 'ApiAuthController@refresh')->name('auth.refresh');
 
-    // Sync
-    Route::get($domainAndModuleParams.'/sync/download', 'SyncController@download')->name('sync.download')->middleware('uccello.permissions:retrieve');
-    Route::post($domainAndModuleParams.'/sync/upload', 'SyncController@upload')->name('sync.upload')->middleware('uccello.permissions:create');
-
     // CRUD
-    Route::get($domainAndModuleParams, 'ApiController@index')->name('index')->middleware('uccello.permissions:retrieve');
-    Route::get($domainAndModuleParams.'/{id}', 'ApiController@show')->name('show')->middleware('uccello.permissions:retrieve');
+    Route::get($domainAndModuleParams, 'ApiController@index')->name('index')->middleware('uccello.permissions:api-retrieve');
+    Route::get($domainAndModuleParams.'/{id}', 'ApiController@show')->name('show')->middleware('uccello.permissions:api-retrieve');
     Route::post($domainAndModuleParams, 'ApiController@store')->name('store')->middleware('uccello.permissions:create');
-    Route::match(['put', 'patch'], $domainAndModuleParams, 'ApiController@update')->name('update')->middleware('uccello.permissions:update');
-    Route::delete($domainAndModuleParams.'/{id}', 'ApiController@destroy')->name('destroy')->middleware('uccello.permissions:delete');
+    Route::match(['put', 'patch'], $domainAndModuleParams, 'ApiController@update')->name('update')->middleware('uccello.permissions:api-update');
+    Route::delete($domainAndModuleParams.'/{id}', 'ApiController@destroy')->name('destroy')->middleware('uccello.permissions:api-delete');
+
+    // Sync
+    Route::get($domainAndModuleParams.'/sync/download', 'SyncController@download')->name('sync.download')->middleware('uccello.permissions:api-retrieve');
+    Route::post($domainAndModuleParams.'/sync/upload', 'SyncController@upload')->name('sync.upload')->middleware('uccello.permissions:api-create');
 });
